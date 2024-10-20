0d4fbd2cfea16a8160864ac1c3dc898b7f2b82a2
unknown <dhjwang@ucdavis.edu>
2024-10-20 16:11:52 -0700

    updated login

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className="test">
       <PlayersProvider>
         <Router>
           <Routes>
